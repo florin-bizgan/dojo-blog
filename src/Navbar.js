e5c3d511fd88ca556383a220c994d84ccdb59453
Florin <florinbizgan2000@gmail.com>
2022-11-14 21:02:41 +0000

    Lesson 6

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,17 @@ const Navbar = () => {
       <h1>The Dojo Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create">New Blog</a>
+        <a
+          href="/create"
+          style={{
+            // Dynamic values used
+            color: "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "8px",
+          }}
+        >
+          New Blog
+        </a>
       </div>
     </nav>
   );
