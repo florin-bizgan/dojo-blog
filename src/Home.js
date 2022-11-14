0780d89b7795d71efabb7d65073aacfece4a44b8
Florin <florinbizgan2000@gmail.com>
2022-11-14 21:17:07 +0000

    Lesson 7

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("Hello", e);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("Hello " + name, e.target);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={(e) => handleClickAgain("Florin", e)}>
+        Click Me Again
+      </button>
     </div>
   );
 };
